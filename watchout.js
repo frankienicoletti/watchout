6a07285fbfd6ce63b34541e66fc58c4cd63871ba
Kate Jefferson <barbedbella@gmail.com>
2015-02-14 12:12:48 -0600

    Improvements to Hero and Enemies

--- watchout.js
+++ watchout.js
@@ -1,9 +1,9 @@
 
-var w = 1000;
-var h = 500;
+var w = 1500;
+var h = 750;
 var enemySet = [];
 var enemyCount = 10;
-var enemyWidth = 5;
+var enemyWidth = 20;
 
 /** Initialize enemySet
  */
@@ -17,8 +17,7 @@ var svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
 
 var drag = d3.behavior.drag()
   .on("drag", function() {
-    d3.select(this).attr("cx",d3.event.x+d3.event.dx);
-    d3.select(this).attr("cy",d3.event.y+d3.event.dy);
+    d3.select(this).attr("cx",d3.event.x+d3.event.dx).attr("cy",d3.event.y+d3.event.dy);
 });
 svg.selectAll("circle")
   .data(enemySet)
@@ -26,20 +25,20 @@ svg.selectAll("circle")
   .append("circle")
   .attr("class", "enemy");
 
-var hero = svg.selectAll("circle.hero").data([1]).enter().append("circle").attr('class','hero').attr("cx", 250)
-  .attr("cy", 250)
-  .attr("r", 10)
+var hero = svg.selectAll("circle.hero").data([1]).enter().append("circle").attr('class','hero').attr("cx", w-20)
+  .attr("cy", h-20)
+  .attr("r", 20)
   .style('fill', 'red').call(drag);
 
 
 function update (data) {
   var allEnemies = svg.selectAll('circle.enemy').data(data);
 
-  allEnemies.attr("cx", function(d, i) {
-    return (Math.floor(Math.random() * 200) + 25);
+  allEnemies.transition().attr("cx", function(d, i) {
+    return (Math.floor(Math.random() * w) + 20);
   })
     .attr("cy", function(d, i) {
-      return (Math.floor(Math.random() * 100) + 25);
+      return (Math.floor(Math.random() * h) + 20);
     })
     .attr("r", function(d) {
       return d;
